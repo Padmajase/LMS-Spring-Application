5a681441f43d1c546e795a4a433ad9ef3934addf
Padmajase <padmajapawar7@gmail.com>
2022-07-09 12:09:55 -0700

    Added : crud operation for users in Learning Management System

---
+++ LMS/src/main/java/com/example/lms/dto/LoginDTO.java
@@ -0,0 +1,11 @@
+package com.example.lms.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginDTO {
+    public String emailId;
+    public String password;
+}
